0e187f2f8d14cc8b37a17c8648d1e1bfea02939a
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-12-05 17:41:10 +0900

    UserモデルとSnsCredentialモデルのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, omniauth_providers: [:google_oauth2]
 
   validates :nickname, presence: true
+  has_many :sns_credentials
 end
