05e1864f8a71490ffda492ecdfd4c84c27028ed8
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-12-05 17:46:25 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  
+  evise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   devise_for :users
   root to: "tweets#index"
   
