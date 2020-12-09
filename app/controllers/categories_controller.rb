ecd74d035bc6cd4688e26dcaacba892385ff1de0
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-12-09 15:26:28 +0900

    カテゴリ別表示のコントローラーの設定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,8 @@
 class CategoriesController < ApplicationController
+
+  def show
+    @categories = Category.all
+    @category = Category.find(params[:id])
+    @tweets = @category.tweets
+  end
 end
