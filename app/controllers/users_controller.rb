302f7a4a7c7f68b369cc7ef4000f6c38923c6cc9
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-12-17 12:38:51 +0900

    userコントローラーの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,4 +27,16 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.followings
+    render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
 end
